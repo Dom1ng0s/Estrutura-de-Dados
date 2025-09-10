ecf65ba73d6cab082357c8e1b7cf9431838c02f7
Davi Domingos de Oliveira <odomingosdavi@gmail.com>
2025-09-10 13:29:12 -0300

    Update ListaDuplamenteEncadeada.c

--- ListaDuplamenteEncadeada.c
+++ ListaDuplamenteEncadeada.c
@@ -6,61 +6,156 @@ typedef struct No {
     struct No* proximo;
     struct No* anterior;
 } No;
+typedef struct Lista
+{
+    No *inicio;
+    No *fim;
+    int tamanho;
+}Lista;
 
-void inserir_no_inicio(No **inicio, int valor) {
-    No *novo = malloc(sizeof(No));
-    novo->valor = valor;
-    novo->proximo = *inicio;
-    novo->anterior = NULL;
-    if (*inicio != NULL) (*inicio)->anterior = novo;
-    *inicio = novo;
+Lista* criar_lista() {
+    Lista *nova_lista = malloc(sizeof(Lista));
+    nova_lista->inicio = NULL;
+    nova_lista->fim = NULL;
+    nova_lista->tamanho = 0;
+    return nova_lista;
 }
-void imprime_do_inicio(No *inicio) {
-    No *atual = inicio;
+
+void inserir_no_inicio(Lista *lista, int valor) {
+    No *novo = malloc(sizeof(No));
+    if (novo) {
+        novo->valor = valor;
+        novo->proximo = lista->inicio; 
+        novo->anterior = NULL;
+        
+        if (lista->inicio != NULL) {
+            lista->inicio->anterior = novo;
+        } else {
+            lista->fim = novo;
+        }
+        
+        lista->inicio = novo; 
+        lista->tamanho++;      
+    }
+}
+
+void inserir_no_final(No **inicio, int valor) {
+    No *aux, *novo = malloc(sizeof(No));
+    if(novo) {
+        novo->valor = valor;
+        novo->proximo = NULL;
+
+        if(*inicio == NULL) {
+            *inicio = novo;
+        } else {
+            aux = *inicio;
+            while(aux->proximo) { 
+                aux = aux->proximo;
+            }
+            aux->proximo = novo;
+            novo->anterior = aux;
+        }
+    }
+}
+
+void imprime_do_inicio(Lista *lista) {
+    printf("Lista do inicio para o fim (%d elementos): ", lista->tamanho);
+    No *atual = lista->inicio; 
     while (atual != NULL) {
         printf("%d ", atual->valor);
         atual = atual->proximo;
     }
+    printf("\n");
 }
-void LiberaLista(No *inicio) {
-    No *atual = inicio;
+
+void liberar_lista(Lista *lista) {
+    No *atual = lista->inicio; 
     while (atual != NULL) {
         No *temp = atual;
         atual = atual->proximo;
         free(temp);
     }
+    free(lista);
 }
-void ImprimeFinal(No *inicio)
-{
-    No *atual = inicio;
-    while(atual && atual-> proximo){
-        atual = atual->proximo;
-    }
+
+void imprime_do_final(Lista *lista) {
+    printf("Lista do fim para o inicio (%d elementos): ", lista->tamanho);
+    No *atual = lista->fim; 
     while (atual != NULL) {
-        printf("%d ",atual->valor);
+        printf("%d ", atual->valor);
         atual = atual->anterior;
     }
-
+    printf("\n");
 }
 
-int main ()
-{
-    int N, i = 0, X;
-    printf("Digite aqui quantos valores você deseja inserir:");
-    scanf("%d",&N);
-    No*inicio = NULL;
-    while ( i < N)
-    {
-        printf("Digite aqui o valor do nó %d:",i+1);
-        scanf("%d",&X);
-        inserir_no_inicio(&inicio,X);
-        
+void exibir_menu() {
+    printf("\n--- MENU - Lista Duplamente Encadeada ---\n");
+    printf("1. Inserir um valor no INÍCIO\n");
+    printf("2. Inserir um valor no FINAL\n");
+    printf("3. Imprimir a lista (do início ao fim)\n");
+    printf("4. Imprimir a lista (do fim ao início)\n");
+    printf("5. Ver o tamanho atual da lista\n");
+    printf("0. Sair do programa\n");
+    printf("Escolha uma opção: ");
+}
 
-        ++i;
-    }
-    imprime_do_inicio(inicio);
-    printf("\n");
-    ImprimeFinal(inicio);
-    LiberaLista(inicio);
+int main() {
+    Lista *minha_lista = criar_lista();
+    int opcao;
+    int valor;
+
+    do {
+        exibir_menu();
+        scanf("%d", &opcao);
+
+        switch (opcao) {
+            case 1:
+                printf("Digite o valor para inserir no início: ");
+                scanf("%d", &valor);
+                inserir_no_inicio(minha_lista, valor);
+                printf("Valor %d inserido com sucesso!\n", valor);
+                break;
+
+            case 2:
+                printf("Digite o valor para inserir no final: ");
+                scanf("%d", &valor);
+                inserir_no_final(minha_lista, valor);
+                printf("Valor %d inserido com sucesso!\n", valor);
+                break;
+
+            case 3:
+                if (minha_lista->inicio == NULL) {
+                    printf("A lista está vazia!\n");
+                } else {
+                    imprime_do_inicio(minha_lista);
+                }
+                break;
+
+            case 4:
+                if (minha_lista->inicio == NULL) {
+                    printf("A lista está vazia!\n");
+                } else {
+                    imprime_do_final(minha_lista);
+                }
+                break;
+            
+            case 5:
+                printf("Tamanho atual da lista: %d\n", minha_lista->tamanho);
+                break;
+
+            case 0:
+                printf("Encerrando o programa...\n");
+                break;
+
+            default:
+                printf("Opção inválida! Tente novamente.\n");
+                break;
+        }
+
+    } while (opcao != 0);
+
+    liberar_lista(minha_lista);
+    printf("Memória liberada. Programa finalizado.\n");
     
+    return 0;
 }
